362b7e47ae38a932299bc031bd79201cf24f9770
esotir <esotir@york.citycollege.eu>
2025-05-20 13:02:21 +0200

    chore: format and lint codebase

--- tests/collisions/preventSelfCollision.test.js
+++ tests/collisions/preventSelfCollision.test.js
@@ -1,4 +1,4 @@
-import { preventSelfCollision } from '../../snake/collisions/preventSelfCollision.js';
+import { preventSelfCollision } from '../../snake/collisions/preventSelfCollision.js'
 
 describe('preventSelfCollision', () => {
   it('marks move unsafe if it leads to self collision', () => {
@@ -7,16 +7,16 @@ describe('preventSelfCollision', () => {
         body: [
           { x: 5, y: 5 },
           { x: 5, y: 4 },
-          { x: 5, y: 3 }
-        ]
-      }
-    };
+          { x: 5, y: 3 },
+        ],
+      },
+    }
 
-    const isMoveSafe = { up: true, down: true, left: true, right: true };
-    const result = preventSelfCollision(gameState, isMoveSafe);
+    const isMoveSafe = { up: true, down: true, left: true, right: true }
+    const result = preventSelfCollision(gameState, isMoveSafe)
 
-    expect(result.down).toBe(false);
-  });
+    expect(result.down).toBe(false)
+  })
 
   it('keeps move safe if it does not lead to self collision', () => {
     const gameState = {
@@ -24,14 +24,14 @@ describe('preventSelfCollision', () => {
         body: [
           { x: 5, y: 5 },
           { x: 5, y: 4 },
-          { x: 4, y: 4 }
-        ]
-      }
-    };
+          { x: 4, y: 4 },
+        ],
+      },
+    }
 
-    const isMoveSafe = { up: true, down: true, left: true, right: true };
-    const result = preventSelfCollision(gameState, isMoveSafe);
+    const isMoveSafe = { up: true, down: true, left: true, right: true }
+    const result = preventSelfCollision(gameState, isMoveSafe)
 
-    expect(result.right).toBe(true);
-  });
-});
+    expect(result.right).toBe(true)
+  })
+})
